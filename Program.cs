23de2de099d8199bf40060c88d55db8ba75c0f6b
Maria Luiza <malualper@hotmail.com>
2020-06-05 22:13:27 -0300

    ultimaparcela

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ultimaparcela
+//Receba o valor total a ser pago e a quantidade de parcelas mensais de um financiamento. Exiba a data e o valor da última parcela.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            Console.Write("Valor total a ser pago: ");
+            int valor=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Quantidade de parcelas: ");
+            int parcelas=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Data atual: ");
+            DateTime data=Convert.ToDateTime(Console.ReadLine());
+
+            DateTime mês=DateTime.Today.AddMonths(parcelas);
+            double UltimaParcela=valor;
+            Console.WriteLine($"Aúltima parcela do seu carnê será em {mês}, no valor de {UltimaParcela:C}");
+    }
+}
+}
